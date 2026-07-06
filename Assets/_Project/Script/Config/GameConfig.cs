ac607054ab0c47329851a99b9eea341e833bb9fc
Nguyễn Tuấn <anhtuan14072002@gmail.com>
2026-07-06 11:10:39 +0700

    test
    test

---
+++ Assets/_Project/Script/Config/GameConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using Core;
+using UnityEngine;
+using Yade.Runtime;
+
+namespace Tuna.Config
+{
+    public partial class GameConfig
+    {
+        private const string ConfigPath = "Configs/";
+
+        public GameConfig()
+        {
+            CallLoadMethods();
+        }
+
+        private void CallLoadMethods()
+        {
+            MethodInfo[] methods = GetType().GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            foreach (var method in methods)
+                if (method.GetCustomAttributes(typeof(LoadConfigAttribute), false).Length > 0)
+                    method.Invoke(this, null);
+        }
+
+        private static YadeSheetData LoadSheet(string sheetName)
+        {
+#if UNITY_EDITOR
+            if (Resources.Load<YadeSheetData>(ConfigPath + sheetName) == null)
+            {
+                new System.IO.DirectoryInfo("Assets/Resources/" + ConfigPath).Create();
+                UnityEditor.AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<YadeSheetData>(),
+                    $"Assets/Resources/{ConfigPath + sheetName}.asset");
+                UnityEditor.AssetDatabase.SaveAssets();
+                UnityEditor.AssetDatabase.Refresh();
+            }
+#endif
+            return Resources.Load<YadeSheetData>(ConfigPath + sheetName);
+        }
+    }
+}
---
+++ Assets/_Project/Script/Config/RateSummon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Core;
+using Yade.Runtime;
+
+namespace Tuna.Config
+{
+    public class RateSummonConfig
+    {
+        [DataField(0)] public int Id;
+        [DataField(1)] public string Rarity;
+        [DataField(2)] public int Level;
+        [DataField(3)] public float Rate;
+        [DataField(4)] public int Price;
+    }
+    public partial class GameConfig
+    {
+        public DictList<int, RateSummonConfig> NameDict;
+        public IEnumerator LoadChapter(string nameConfig)
+        {
+            NameDict = LoadSheet(nameConfig).AsDictList<int, RateSummonConfig>(key => key.Id);
+            yield return null;
+        }
+        public List<RateSummonConfig> LoadNameConfig(int id)
+        {
+            if (NameDict.TryGetValue(id, out var config))
+                return config;
+            return new List<RateSummonConfig>();
+        }
+    }
+}
